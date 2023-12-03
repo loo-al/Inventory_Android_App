e375f712a832feec57e7daa02c3366e8f5ddb831
loo_al <alexander.loo01@student.csulb.edu>
2023-12-02 22:08:45 -0800

    Update Compile Version

--- InventoryApp/app/build.gradle.kts
+++ InventoryApp/app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.inventory"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
